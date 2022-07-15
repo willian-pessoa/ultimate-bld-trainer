b619a8aafe8be344484a84b785dfd33be65f41d8
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-07-15 12:38:44 -0300

    change the text home page

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./home.styles.scss";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className="home-page-container">
+      <h4>Welcome to The Comms Reviewer</h4>
+      <div className="description-home-page">
+        <p>
+          A web app to help you review commutators and stick them well in memory.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
