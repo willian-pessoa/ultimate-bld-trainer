054b45035f79f10a536cc9fb62ec774b97a2a8e7
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-07-15 13:48:21 -0300

    converted into a pwa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -25,6 +26,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
